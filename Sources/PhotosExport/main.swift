b5345cb3b52a5bd7c2890f393486f9e2ca9258b4
Rui Carmo <rui.carmo@gmail.com>
2025-12-27 15:28:09 +0000

    Enhance metadata extraction and JSON handling for exported photos

--- Sources/PhotosExport/main.swift
+++ Sources/PhotosExport/main.swift
@@ -2,6 +2,8 @@ import Foundation
 import Photos
 import ImageIO
 import UniformTypeIdentifiers
+import CoreLocation
+import AVFoundation
 
 actor LineLogger {
   private let handle: FileHandle
@@ -62,6 +64,68 @@ func alphaLetter(from hash: UInt64, offset: Int = 0) -> Character {
   return Character(UnicodeScalar(97 + idx)!)
 }
 
+func jsonSafe(_ value: Any) -> Any {
+  switch value {
+  case let v as String: return v
+  case let v as NSNumber: return v
+  case is NSNull: return value
+  case let v as Date: return isoTimestamp(v)
+  case let v as URL: return v.absoluteString
+  case let v as Data: return v.base64EncodedString()
+  case let v as [Any]: return v.map { jsonSafe($0) }
+  case let v as [String: Any]: return jsonSafeDict(v)
+  default: return String(describing: value)
+  }
+}
+
+func jsonSafeDict(_ dict: [String: Any]) -> [String: Any] {
+  var out: [String: Any] = [:]
+  for (k, v) in dict {
+    out[k] = jsonSafe(v)
+  }
+  return out
+}
+
+func avMetadataItemsToJSON(_ items: [AVMetadataItem]) async -> [[String: Any]] {
+  var result: [[String: Any]] = []
+  result.reserveCapacity(items.count)
+
+  for item in items {
+    var entry: [String: Any] = [:]
+    if let id = item.identifier?.rawValue { entry["identifier"] = id }
+    if let key = item.key?.description { entry["key"] = key }
+    if let common = item.commonKey?.rawValue { entry["commonKey"] = common }
+
+    if #available(macOS 13.0, *) {
+      if let stringValue = try? await item.load(.stringValue) {
+        entry["value"] = stringValue
+      } else if let numberValue = try? await item.load(.numberValue) {
+        entry["value"] = numberValue
+      } else if let dateValue = try? await item.load(.dateValue) {
+        entry["value"] = isoTimestamp(dateValue)
+      } else if let dataValue = try? await item.load(.dataValue) {
+        entry["valueBase64"] = dataValue.base64EncodedString()
+      }
+    } else {
+      if let stringValue = item.stringValue {
+        entry["value"] = stringValue
+      } else if let numberValue = item.numberValue {
+        entry["value"] = numberValue
+      } else if let dateValue = item.dateValue {
+        entry["value"] = isoTimestamp(dateValue)
+      } else if let dataValue = item.dataValue {
+        entry["valueBase64"] = dataValue.base64EncodedString()
+      }
+    }
+
+    if !entry.isEmpty {
+      result.append(entry)
+    }
+  }
+
+  return result
+}
+
 func exportFilename(
   captureDate: Date,
   originalFilename: String,
@@ -110,6 +174,7 @@ struct Settings {
   var logFile: URL? = nil
   var debug: Bool = false
   var incremental: Bool = false
+  var metadata: Bool = false
 }
 
 func parseSettings(_ args: [String]) -> Settings {
@@ -122,6 +187,9 @@ func parseSettings(_ args: [String]) -> Settings {
       i += 1
     case "--incremental":
       settings.incremental = true
+      i += 1
+    case "--metadata":
+      settings.metadata = true
       i += 1
     case "--log-file":
       if i + 1 < args.count {
@@ -377,19 +445,210 @@ func filenameForResource(asset: PHAsset, resource: PHAssetResource, captureDate:
   )
 }
 
+func extractMetadata(asset: PHAsset, logger: LineLogger? = nil) async -> [String: Any] {
+  var metadata: [String: Any] = [:]
+
+  // Basic asset information
+  metadata["localIdentifier"] = asset.localIdentifier
+  metadata["mediaType"] = asset.mediaType.rawValue
+  metadata["mediaSubtypes"] = asset.mediaSubtypes.rawValue
+  metadata["pixelWidth"] = asset.pixelWidth
+  metadata["pixelHeight"] = asset.pixelHeight
+  metadata["duration"] = asset.duration
+  metadata["favorite"] = asset.isFavorite
+  metadata["hidden"] = asset.isHidden
+  metadata["playbackStyle"] = asset.playbackStyle.rawValue
+  metadata["isLivePhoto"] = asset.mediaSubtypes.contains(.photoLive)
+  metadata["isPanorama"] = asset.mediaSubtypes.contains(.photoPanorama)
+  metadata["isHDR"] = asset.mediaSubtypes.contains(.photoHDR)
+  metadata["isScreenshot"] = asset.mediaSubtypes.contains(.photoScreenshot)
+  metadata["isDepthEffect"] = asset.mediaSubtypes.contains(.photoDepthEffect)
+  metadata["isHighFrameRateVideo"] = asset.mediaSubtypes.contains(.videoHighFrameRate)
+  metadata["isTimeLapseVideo"] = asset.mediaSubtypes.contains(.videoTimelapse)
+
+  // Dates
+  if let creationDate = asset.creationDate { metadata["creationDate"] = isoTimestamp(creationDate) }
+  if let modificationDate = asset.modificationDate { metadata["modificationDate"] = isoTimestamp(modificationDate) }
+
+  // Location + reverse geocode
+  if let location = asset.location {
+    var locationInfo: [String: Any] = [:]
+    locationInfo["latitude"] = location.coordinate.latitude
+    locationInfo["longitude"] = location.coordinate.longitude
+    if location.altitude != 0 { locationInfo["altitude"] = location.altitude }
+    if location.speed >= 0 { locationInfo["speed"] = location.speed }
+    if location.course >= 0 { locationInfo["course"] = location.course }
+    if location.horizontalAccuracy >= 0 { locationInfo["horizontalAccuracy"] = location.horizontalAccuracy }
+    if location.verticalAccuracy >= 0 { locationInfo["verticalAccuracy"] = location.verticalAccuracy }
+    metadata["location"] = locationInfo
+
+    do {
+      let geocoder = CLGeocoder()
+      let placemarks = try await geocoder.reverseGeocodeLocation(location)
+      if let placemark = placemarks.first {
+        var place: [String: Any] = [:]
+        if let v = placemark.name { place["name"] = v }
+        if let v = placemark.locality { place["locality"] = v }
+        if let v = placemark.subLocality { place["subLocality"] = v }
+        if let v = placemark.administrativeArea { place["administrativeArea"] = v }
+        if let v = placemark.subAdministrativeArea { place["subAdministrativeArea"] = v }
+        if let v = placemark.postalCode { place["postalCode"] = v }
+        if let v = placemark.country { place["country"] = v }
+        if let v = placemark.isoCountryCode { place["isoCountryCode"] = v }
+        if let v = placemark.ocean { place["ocean"] = v }
+        if let v = placemark.inlandWater { place["inlandWater"] = v }
+        if let v = placemark.areasOfInterest, !v.isEmpty { place["areasOfInterest"] = v }
+        if !place.isEmpty { metadata["placemark"] = place }
+      }
+    } catch {
+      await logDebug(logger, "metadata.geocode failed asset=\(asset.localIdentifier) error=\(error)")
+    }
+  }
+
+  // Source type and burst
+  metadata["sourceType"] = asset.sourceType.rawValue
+  if let burstIdentifier = asset.burstIdentifier {
+    metadata["burstIdentifier"] = burstIdentifier
+    metadata["burstSelectionTypes"] = asset.burstSelectionTypes.rawValue
+  }
+
+  // Resources summary
+  let resources = PHAssetResource.assetResources(for: asset)
+  metadata["resources"] = resources.map { r in
+    [
+      "type": r.type.rawValue,
+      "originalFilename": r.originalFilename,
+      "uniformTypeIdentifier": r.uniformTypeIdentifier
+    ]
+  }
+
+  // Fetch content editing input
+  let contentInput: PHContentEditingInput? = await withCheckedContinuation { (cont: CheckedContinuation<PHContentEditingInput?, Never>) in
+    let opts = PHContentEditingInputRequestOptions()
+    opts.isNetworkAccessAllowed = true
+    asset.requestContentEditingInput(with: opts) { input, _ in
+      cont.resume(returning: input)
+    }
+  }
+  guard let input = contentInput else { return metadata }
+
+  metadata["contentUniformType"] = input.uniformTypeIdentifier
+  metadata["fullSizeImageOrientation"] = input.fullSizeImageOrientation
+  if let pairing = input.value(forKey: "livePhotoPairingIdentifier") as? String {
+    metadata["livePhotoPairingIdentifier"] = pairing
+  }
+  if let adjustment = input.adjustmentData {
+    metadata["adjustmentData"] = [
+      "formatIdentifier": adjustment.formatIdentifier,
+      "formatVersion": adjustment.formatVersion,
+      "dataBase64": adjustment.data.base64EncodedString()
+    ]
+  }
+
+  // AV metadata
+  if let avAsset = input.audiovisualAsset {
+    if #available(macOS 13.0, *) {
+      if let duration = try? await avAsset.load(.duration) {
+        metadata["avAssetDurationSeconds"] = CMTimeGetSeconds(duration)
+      }
+      if let tracks = try? await avAsset.loadTracks(withMediaType: .video), let videoTrack = tracks.first {
+        if let size = try? await videoTrack.load(.naturalSize) {
+          metadata["avVideoDimensions"] = ["width": size.width, "height": size.height]
+        }
+      }
+      if let items = try? await avAsset.load(.metadata) {
+        metadata["avMetadata"] = await avMetadataItemsToJSON(items)
+      }
+    } else {
+      metadata["avAssetDurationSeconds"] = CMTimeGetSeconds(avAsset.duration)
+      if let videoTrack = avAsset.tracks(withMediaType: .video).first {
+        metadata["avVideoDimensions"] = [
+          "width": videoTrack.naturalSize.width,
+          "height": videoTrack.naturalSize.height
+        ]
+      }
+      metadata["avMetadata"] = await avMetadataItemsToJSON(avAsset.metadata)
+    }
+  }
+
+  // ImageIO metadata
+  if let url = input.fullSizeImageURL,
+     let imageSource = CGImageSourceCreateWithURL(url as CFURL, nil),
+     let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as? [String: Any] {
+    metadata["imageProperties"] = jsonSafeDict(imageProperties)
+
+    if let exif = imageProperties[kCGImagePropertyExifDictionary as String] as? [String: Any] {
+      let safeExif = jsonSafeDict(exif)
+      metadata["exif"] = safeExif
+      var camera: [String: Any] = metadata["camera"] as? [String: Any] ?? [:]
+      if let v = exif["LensModel"] as? String { camera["lensModel"] = v }
+      if let v = exif["LensMake"] as? String { camera["lensMake"] = v }
+      if let v = exif["LensSerialNumber"] as? String { camera["lensSerialNumber"] = v }
+      if let v = exif["BodySerialNumber"] as? String { camera["bodySerialNumber"] = v }
+      if !camera.isEmpty { metadata["camera"] = camera }
+    }
+
+    if let gps = imageProperties[kCGImagePropertyGPSDictionary as String] as? [String: Any] {
+      metadata["gps"] = jsonSafeDict(gps)
+    }
+
+    if let tiff = imageProperties[kCGImagePropertyTIFFDictionary as String] as? [String: Any] {
+      let safeTiff = jsonSafeDict(tiff)
+      metadata["tiff"] = safeTiff
+      var camera = metadata["camera"] as? [String: Any] ?? [:]
+      if let v = tiff["Model"] as? String { camera["model"] = v }
+      if let v = tiff["Make"] as? String { camera["make"] = v }
+      if !camera.isEmpty { metadata["camera"] = camera }
+    }
+
+    if let iptc = imageProperties[kCGImagePropertyIPTCDictionary as String] as? [String: Any] {
+      let safeIptc = jsonSafeDict(iptc)
+      metadata["iptc"] = safeIptc
+      if let keywords = iptc[kCGImagePropertyIPTCKeywords as String] as? [String], !keywords.isEmpty {
+        metadata["keywords"] = keywords
+      }
+      if let caption = iptc[kCGImagePropertyIPTCCaptionAbstract as String] as? String, !caption.isEmpty {
+        metadata["caption"] = caption
+      }
+    }
+
+    if let xmp = imageProperties["{XMP}"] as? [String: Any] {
+      metadata["xmp"] = jsonSafeDict(xmp)
+    }
+
+    if let makerNote = imageProperties[kCGImagePropertyMakerAppleDictionary as String] as? [String: Any] {
+      metadata["makerApple"] = jsonSafeDict(makerNote)
+    }
+  }
+
+  return metadata
+}
+
+func saveMetadataJSON(metadata: [String: Any], to url: URL, logger: LineLogger? = nil) async throws {
+  let jsonData = try JSONSerialization.data(withJSONObject: metadata, options: [.prettyPrinted, .sortedKeys])
+  try jsonData.write(to: url)
+
+  if let attrs = try? FileManager.default.attributesOfItem(atPath: url.path),
+     let size = attrs[.size] as? NSNumber {
+    await logDebug(logger, "metadata.json.saved path=\(url.path) bytes=\(size.int64Value)")
+  } else {
+    await logDebug(logger, "metadata.json.saved path=\(url.path)")
+  }
+}
+
 func exportAllResources(
   asset: PHAsset,
   captureDate: Date,
   to folder: URL,
   incremental: Bool,
   errorLogURL: URL,
-  logger: LineLogger? = nil
-) async throws -> [URL] {
+  logger: LineLogger? = nil,
+  usedNames: inout Set<String>
+) async throws -> [(PHAssetResource, URL)] {
   let resources = PHAssetResource.assetResources(for: asset)
   await logDebug(logger, "asset.resources count=\(resources.count) asset=\(asset.localIdentifier)")
 
-  var usedNames = Set<String>()
-  var exported: [URL] = []
+  var exported: [(PHAssetResource, URL)] = []
   exported.reserveCapacity(resources.count)
 
   var failures: [String] = []
@@ -404,7 +663,7 @@ func exportAllResources(
 
     if incremental && FileManager.default.fileExists(atPath: finalURL.path) {
       await logDebug(logger, "asset.resource.skip existing asset=\(asset.localIdentifier) type=\(typeLabel) dest=\(finalURL.path)")
-      exported.append(finalURL)
+      exported.append((r, finalURL))
       continue
     }
 
@@ -467,7 +726,7 @@ func exportAllResources(
       await logDebug(logger, "asset.resource.done asset=\(asset.localIdentifier) type=\(typeLabel) path=\(finalURL.path)")
     }
 
-    exported.append(finalURL)
+    exported.append((r, finalURL))
   }
 
   if !failures.isEmpty {
@@ -578,14 +837,49 @@ enum Main {
           try await ensureDir(folder, logger: debugLogger)
           await logDebug(debugLogger, "asset.folder asset=\(asset.localIdentifier) folder=\(folder.path)")
 
-          _ = try await exportAllResources(
+          var usedNames = Set<String>()
+          let exportedResources = try await exportAllResources(
             asset: asset,
             captureDate: date,
             to: folder,
             incremental: settings.incremental,
             errorLogURL: errorLog,
-            logger: debugLogger
+            logger: debugLogger,
+            usedNames: &usedNames
           )
+
+          if settings.metadata {
+            let metaOriginal = exportedResources.first?.0.originalFilename ?? ""
+            let sidecarName = exportFilename(
+              captureDate: date,
+              originalFilename: metaOriginal,
+              fallbackSeed: "asset=\(asset.localIdentifier)|metadata",
+              uti: UTType.json.identifier,
+              usedNames: &usedNames
+            )
+            let sidecarURL = folder.appendingPathComponent(sidecarName)
+
+            var metadata = await extractMetadata(asset: asset, logger: debugLogger)
+
+            // Attach exported file info (type, filenames, sizes) to metadata payload.
+            metadata["exportedFiles"] = exportedResources.map { res, url in
+              var entry: [String: Any] = [
+                "type": res.type.rawValue,
+                "uniformTypeIdentifier": res.uniformTypeIdentifier,
+                "originalFilename": res.originalFilename,
+                "exportedFilename": url.lastPathComponent,
+                "path": url.path
+              ]
+              if let attrs = try? FileManager.default.attributesOfItem(atPath: url.path) {
+                if let size = attrs[.size] as? NSNumber { entry["fileSize"] = size }
+                if let mod = attrs[.modificationDate] as? Date { entry["fileModificationDate"] = isoTimestamp(mod) }
+              }
+              return entry
+            }
+
+            try await saveMetadataJSON(metadata: metadata, to: sidecarURL, logger: debugLogger)
+          }
+
           exported += 1
           bar.tick(label + " ✓")
         } catch {
